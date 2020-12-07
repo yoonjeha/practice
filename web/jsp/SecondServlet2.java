10213ae9a16172733ec74b76bd2782bc0b30f083
JiyoonHan <67572883+yoonjeha@users.noreply.github.com>
2020-12-07 22:39:04 +0900

    Add files via upload

---
+++ web/jsp/SecondServlet2.java
@@ -0,0 +1,35 @@
+package webproj;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/second2")
+public class SecondServlet2 extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    public SecondServlet2() {
+        super();
+            }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    response.setContentType("text/html; charset=utf-8");
+    PrintWriter out = response.getWriter();
+    out.println("<html><body>");
+    out.println("sendRedirect를 이용한 redirect 실습입니다. ");
+    //브라우저로 출력합니다.
+    out.println("</body></html>");
+    }
+
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+}
