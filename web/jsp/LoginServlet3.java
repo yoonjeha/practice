557555578f4b3924f1e5b031f520945825ad64c6
JiyoonHan <67572883+yoonjeha@users.noreply.github.com>
2020-12-10 16:31:59 +0900

    Add files via upload

---
+++ web/jsp/LoginServlet3.java
@@ -0,0 +1,57 @@
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.URLEncoder;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+@WebServlet("/LoginServlet3")
+public class LoginServlet3 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doHandle(request, response);
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doHandle(request, response);
+	}
+	private void doHandle(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+		String user_id = request.getParameter("user_id");
+		String user_pwd = request.getParameter("user_pwd");
+		
+		MemberVO memberVO = new MemberVO();
+		memberVO.setId(user_id);
+		memberVO.setPwd(user_pwd);
+		MemberDAO6 dao = new MemberDAO6();
+		boolean result = dao.isExisted(memberVO);
+		
+		if (result) {
+			HttpSession session = request.getSession();
+			session.setAttribute("isLogon", true);
+			session.setAttribute("login.id", user_id);
+			session.setAttribute("login.pwd", user_pwd);
+			
+			out.print("<html><body>");
+			out.print("안녕하세요" + user_id + "님 ! ! !  <br>");
+			out.print("<a href='show'>회원정보보기</a>");
+			out.print("</body></html>");
+		} else {
+			out.print("<html><body>회원 아이디가 틀립니다.");
+			out.print("<a href='login3.html'>다시 로그인 하기</a>");
+			out.print("</body></html>");
+		}
+	}
+}
+/*
+ * 톰캣을 재 실행한 후 로그인창을 거치지 않고 바로 /show로 요청한 경우
+ * 로그인을 하지 않았으므로 다시 로그인창으로 리다이렉트 됩니다.
+ */
